eef0147ef05e3efa3fa684ec7e06b68ba52eea46
OlawaleP <olawaleojo7j@gmail.com>
2025-07-05 20:51:12 +0100

    Fix: updated the the app name

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -11,7 +11,7 @@ export const Header: React.FC = () => {
       <div className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           <Link href="/" className="text-2xl font-bold text-gray-900">
-            Stackbuld-Commerce
+            Stackbuld
           </Link>
           
           <div className="flex-1 mx-4">
