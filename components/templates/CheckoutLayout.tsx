f8587fc0b48812fccc328a6b176f47617cacc2ef
OlawaleP <olawaleojo7j@gmail.com>
2025-07-05 12:17:17 +0100

    Feat: checkout layout

---
+++ components/templates/CheckoutLayout.tsx
@@ -0,0 +1,23 @@
+import { Header } from '@/components/organisms/Header';
+import { Footer } from '@/components/organisms/Footer';
+import { SEO } from '@/components/atoms/SEO';
+
+interface CheckoutLayoutProps {
+  children: React.ReactNode;
+}
+
+export const CheckoutLayout: React.FC<CheckoutLayoutProps> = ({ children }) => {
+  return (
+    <>
+      <SEO 
+        title="Checkout"
+        description="Complete your purchase at Mini-Commerce"
+      />
+      <div className="flex min-h-screen flex-col">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
+    </>
+  );
+};
