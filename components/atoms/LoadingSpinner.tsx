d76dd030ef173064fff1e6e9694a6d7c1da5873c
OlawaleP <olawaleojo7j@gmail.com>
2025-07-05 19:22:18 +0100

    Fix: update the loading spinner to use data-testid

--- components/atoms/LoadingSpinner.tsx
+++ components/atoms/LoadingSpinner.tsx
@@ -1,31 +1,25 @@
 import { cn } from '@/lib/utils';
-import { forwardRef, HTMLAttributes } from 'react';
 
-interface LoadingSpinnerProps extends HTMLAttributes<HTMLDivElement> {
+interface LoadingSpinnerProps {
   size?: 'sm' | 'md' | 'lg';
+  className?: string;
 }
 
-const LoadingSpinner = forwardRef<HTMLDivElement, LoadingSpinnerProps>(
-  ({ className, size = 'md', ...props }, ref) => {
-    const sizes = {
-      sm: 'h-4 w-4 border-2',
-      md: 'h-6 w-6 border-3',
-      lg: 'h-8 w-8 border-4',
-    };
+export const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ size = 'md', className }) => {
+  const sizeClasses = {
+    sm: 'h-4 w-4',
+    md: 'h-6 w-6',
+    lg: 'h-8 w-8',
+  };
 
-    return (
-      <div
-        ref={ref}
-        className={cn(
-          'animate-spin rounded-full border-t-transparent',
-          sizes[size],
-          className
-        )}
-        {...props}
-      />
-    );
-  }
-);
-
-LoadingSpinner.displayName = 'LoadingSpinner';
-export { LoadingSpinner };
+  return (
+    <div
+      data-testid="loading-spinner"
+      className={cn(
+        'animate-spin rounded-full border-2 border-t-blue-600 border-gray-300',
+        sizeClasses[size],
+        className
+      )}
+    />
+  );
+};
