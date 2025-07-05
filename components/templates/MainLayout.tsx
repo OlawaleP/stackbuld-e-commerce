a29c752debfd00896a8ccd4197a694e46c89b20d
OlawaleP <olawaleojo7j@gmail.com>
2025-07-05 12:16:28 +0100

    Feat: main layout

---
+++ components/templates/MainLayout.tsx
@@ -0,0 +1,26 @@
+import { Header } from '@/components/organisms/Header';
+import { Footer } from '@/components/organisms/Footer';
+import { SEO } from '@/components/atoms/SEO';
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+  title?: string;
+  description?: string;
+}
+
+export const MainLayout: React.FC<MainLayoutProps> = ({
+  children,
+  title,
+  description,
+}) => {
+  return (
+    <>
+      <SEO title={title} description={description} />
+      <div className="flex min-h-screen flex-col">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
+    </>
+  );
+};
