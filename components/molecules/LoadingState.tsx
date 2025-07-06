194d1e5d2d7459381b43de0f226d47b2ed9b4583
OlawaleP <olawaleojo7j@gmail.com>
2025-07-06 16:52:30 +0100

    Feat: added loading state

---
+++ components/molecules/LoadingState.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { LoadingSpinner } from '@/components/atoms/LoadingSpinner';
+
+interface LoadingStateProps {
+  message?: string;
+}
+
+export const LoadingState: React.FC<LoadingStateProps> = ({ 
+  message = 'Loading...' 
+}) => {
+  return (
+    <div className="flex min-h-[400px] items-center justify-center">
+      <div className="text-center">
+        <LoadingSpinner size="lg" />
+        <p className="mt-4 text-gray-600">{message}</p>
+      </div>
+    </div>
+  );
+};
