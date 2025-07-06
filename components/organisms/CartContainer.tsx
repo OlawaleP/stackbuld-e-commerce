325d0187e82e98838f983c0bcf8e0cfe725b9300
OlawaleP <olawaleojo7j@gmail.com>
2025-07-06 16:27:52 +0100

    Fix: refactor the checkout codebase

---
+++ components/organisms/CartContainer.tsx
@@ -0,0 +1,30 @@
+// components/organisms/CartContainer.tsx
+'use client';
+
+import { useCartStore } from '@/lib/store/cart-store';
+import { EmptyCart } from '@/components/molecules/EmptyCart';
+import { OrderSummary } from '@/components/organisms/OrderSummary';
+import { CartHeader } from '../molecules/CartHeader';
+import { CartItemsList } from './CartItemsList';
+
+export function CartContainer() {
+  const { items, getItemCount } = useCartStore();
+
+  if (items.length === 0) {
+    return <EmptyCart />;
+  }
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <CartHeader itemCount={getItemCount()} />
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+        <div className="lg:col-span-2">
+          <CartItemsList /> 
+        </div>
+        <div className="lg:col-span-1">
+          <OrderSummary />
+        </div>
+      </div>
+    </div>
+  );
+}
