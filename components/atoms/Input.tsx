bbc9c86f0aa4177a7aedeef236a52744ac510b5b
OlawaleP <olawaleojo7j@gmail.com>
2025-07-07 08:58:52 +0100

    Fix: refactored the code section

--- components/atoms/Input.tsx
+++ components/atoms/Input.tsx
@@ -25,7 +25,6 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
             'flex h-10 w-full rounded-md border px-3 py-2 text-sm',
             'focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500',
             'transition-colors duration-200',
-            // Better autofill styling
             'autofill:bg-blue-50 autofill:text-blue-900',
             error ? 'border-red-500 focus:ring-red-500' : 'border-gray-300',
             className
